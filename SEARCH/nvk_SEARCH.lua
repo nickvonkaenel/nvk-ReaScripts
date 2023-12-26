59541a8ff617837a706d04e1b71aba43b6982b84
NickvonKaenel <52364921+NickvonKaenel@users.noreply.github.com>
2023-12-26 00:22:50 -0800

    Committing changes from script

---
+++ SEARCH/nvk_SEARCH.lua
@@ -0,0 +1,27 @@
+--[[
+Description: nvk_SEARCH
+Version: 1.0.0
+About:
+  # nvk_SEARCH
+
+  This script is used to quickly search for FX, chains, actions, projects, etc in Reaper. Requires REAPER 7 or higher.
+Author: nvk
+Links:
+  REAPER forum thread https://forum.cockos.com/showthread.php?t=259057
+Provides:
+  **/*.dat
+  **/*.otf
+  [main] *.lua
+--]]
+SCRIPT_FOLDER = 'search'
+r = reaper
+if not r.APIExists('EnumInstalledFX') then
+    r.MB('Please update to REAPER 7 or higher to use the script.', 'nvk_SEARCH', 0)
+    return
+end
+sep = package.config:sub(1, 1)
+DATA = _VERSION == 'Lua 5.3' and 'Data53' or 'Data'
+DATA_PATH = debug.getinfo(1, 'S').source:match("@(.+[/\\])") .. DATA .. sep
+dofile(DATA_PATH .. 'functions.dat')
+if not functionsLoaded then return end
+
