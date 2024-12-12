6a944c07dfec0f7b0cfdd73babbea22f987c23a7
NickvonKaenel <52364921+NickvonKaenel@users.noreply.github.com>
2024-12-12 08:36:06 -0800

    Committing changes from script

--- TAKES/nvk_TAKES.lua
+++ TAKES/nvk_TAKES.lua
@@ -1,6 +1,6 @@
 --[[
 Description: nvk_TAKES
-Version: 2.5.4
+Version: 2.5.5
 About:
     # nvk_TAKES
 
@@ -10,6 +10,8 @@ Links:
     Store Page https://gum.co/nvk_WORKFLOW
     User Guide https://nvk.tools/docs/workflow/takes
 Changelog:
+    2.5.5
+        Error when running quick add numbered take marker script
     2.5.4
         Crash with render to new take with fx when more than 5 items are selected
     2.5.3
